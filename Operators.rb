f949e1ac6ac0931e31f77cab581d197379dc2d9e
Alin Craciunescu <alin.craciunescu@yahoo.com>
2018-09-13 18:14:05 +0300

    Operators

--- Operators.rb
+++ Operators.rb
@@ -10,3 +10,30 @@ c = 10;
 p a == c;
 p a == b;
 p c == a;
+
+
+p 5 == 5.0;
+
+
+# != inequality operator
+
+p "hello" != "hello";
+p "hello" != "Hello";
+
+p "hello".downcase != "HeLlo".downcase
+
+p "123" != 123;
+
+# < and > do the same thing as in pute mathematics
+# <= less or equal ---> greater or equal to >= ....
+p 1 < 3;
+p 2 <= 2;
+p 12 <= 11;
+
+# aritmatic operators 
+# 	+ plus
+# 	- minus
+# 	/ divided
+# 	* times
+# 	% modulo
+
