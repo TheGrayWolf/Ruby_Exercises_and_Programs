efc7ae30c845435e941b3020067ce2845b394ea9
Alin Craciunescu <alin.craciunescu@yahoo.com>
2018-09-13 10:15:43 +0300

    update

--- objects.rb
+++ objects.rb
@@ -10,5 +10,17 @@ p "Hello World".upcase
 # Downcase sets all the letters in a string to lowercase
 p "Hello World".downcase
 
+# We declare a variable he
 foo = "Hello World";
 p foo.length;
+p foo.upcase;
+
+# Next adds 1
+p 10.next;
+
+p -1.next;
+
+puts "Hello World";
+puts "Hello World".inspect;
+
+p "Hello World";
