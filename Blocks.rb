ca31ab43858029165805f42a02dc15edefd52603
Alin Craciunescu <alin.craciunescu@yahoo.com>
2018-09-14 18:21:58 +0300

    Blocks

---
+++ Blocks.rb
@@ -0,0 +1,17 @@
+# Blocks
+
+5.times
+
+#times executes the method x times
+
+
+# Between curly braces {} is the BLOCK
+
+10.times { puts "Alin is AWESOME ... sometimes ... very rarely ..."};
+
+# Using the Keywords DO and END;
+2.times do 
+	puts "potatooooooo";
+	puts "I LOVE POTATOOOOOO";
+	puts "For The Horde"
+end
