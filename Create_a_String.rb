afbc8cfe41eead575f349c34ef56019c7c26a8e9
Alin Craciunescu <alin.craciunescu@yahoo.com>
2018-09-18 16:26:32 +0300

    Strings

---
+++ Create_a_String.rb
@@ -0,0 +1,17 @@
+# Strings are represented by the double quotes => ""
+# String means that everithing is treated as text as long as it is between quotes ""
+# Strings can be assigned to variables
+puts "Hi there, I am a string"
+puts "A String can include characters and numbers even a space :# $ % 123123 a. fasd asd asd dasda "
+
+name = "PotatoKing" 
+revenue = "12 $"
+puts name , revenue;
+
+space = " "
+p space.length
+
+puts
+
+empnty = ""
+p empnty.length;
