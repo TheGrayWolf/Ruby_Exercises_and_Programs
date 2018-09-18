7c1064931b939f56cf5ccfe1ba7cbb98ae0f1c9e
Alin Craciunescu <alin.craciunescu@yahoo.com>
2018-09-18 16:25:29 +0300

    Blocks update

--- Blocks.rb
+++ Blocks.rb
@@ -8,6 +8,7 @@
 # Between curly braces {} is the BLOCK
 
 10.times { puts "Alin is AWESOME ... sometimes ... very rarely ..."};
+puts "**********************"
 
 # Using the Keywords DO and END;
 2.times do 
@@ -15,11 +16,13 @@
 	puts "I LOVE POTATOOOOOO";
 	puts "For The Horde"
 end
+puts "**********************"
 # | | -> block variables
 2.times do |i|
 	puts "we are currently on loop number #{i}";
 	puts "For The Horde"
 end
+puts "**********************"
 
 #      ***** EXERCISE *****
 
@@ -29,10 +32,31 @@ end
 	puts 3 * (i+1)
 }
 
+puts "**********************"
+
 10.times do |i|
 	puts 3 * (i+1);
 end
+puts "**********************"
 
 #     ***** UPTO & DOWNTO *****
 
-5.downto(1){}
+5.downto(1){ |i| puts "Countdown: #{i}" }
+
+puts "**********************"
+
+5.downto(1) do |i|
+	puts "Countdown: #{i}"
+	end
+
+puts "**********************"
+
+
+5.upto(10) { |i| puts "Countup #{i}"}
+
+puts "**********************"
+
+5.upto(10) do |i|
+	puts "Countup #{i}"
+	end
+# ***** UPTO & DOWN TO re-writen *****
