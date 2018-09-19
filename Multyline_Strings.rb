03358de41979a6f5c85b6945185f5364ecbb3769
Alin Craciunescu <alin.craciunescu@yahoo.com>
2018-09-19 11:27:48 +0300

    Multyple String Lines In Ruby

---
+++ Multyline_Strings.rb
@@ -0,0 +1,15 @@
+# Multiple Strings or Miltiple Lines
+
+
+# To write multiple lines use the thow less than signs << followed by the unique identifyer and close the text with the unique identifyer
+
+words = <<MLS
+	this ia potato
+	this is tomato
+	tabs are preserved
+	Gumbye!
+
+
+Ha ha !! gotcha
+MLS
+print words
