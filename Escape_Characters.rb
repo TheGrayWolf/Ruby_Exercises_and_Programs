d5642405e633c877d42b2da0c2761c64d2d0a028
Alin Craciunescu <alin.craciunescu@yahoo.com>
2018-09-19 11:48:18 +0300

    Ruby escape characters

---
+++ Escape_Characters.rb
@@ -0,0 +1,40 @@
+
+# Special bits of sintax that allow us to manipulate Strings
+
+#For example: "" and '' you can use thse to put "" or '' in a String
+
+# EXAMPLE:
+
+puts "i said 'Goodbye' to fats";
+puts 'I said "Goodbye" to fats';
+
+puts
+
+# To use same Quotes we need to tell ruby to escape these by putting a backslash \ before the quotes
+
+# EXAMPLE:
+
+puts "I said \"Goodbye\" to fats";
+puts 'I said \'Goodbye\' to fats';
+
+puts
+
+# Line Brakes -> to force a line brake in Ruby we use \n 
+
+# EXAMPLE:
+
+result = "Let's add a line break \nright here";
+
+puts result;
+
+puts
+
+p result;
+
+puts
+
+# Tabs ( indented space at the begining of the parafraph ) to put a Tab we use \t 
+
+# EXAMPLE:
+
+puts "\tI Love Potatoes";
