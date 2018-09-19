510679ec5ea6c605624faef6c7ecdb676fef3580
Alin Craciunescu <alin.craciunescu@yahoo.com>
2018-09-19 16:52:43 +0300

    Single vs Double Quotes
    What are the differences between them

---
+++ Single_vs_Double_quotes.rb
@@ -0,0 +1,34 @@
+
+# In double Quotes you can use Line Brakes
+
+# EXAMPLE
+
+puts "Hello\nWorld";
+
+puts
+
+# In Double Quotes you can use Interpolations
+
+# EXAMPLE
+
+text = "We all live in a yelow submarine";
+puts "#{text}";
+
+puts
+
+
+#***********************************
+
+
+# In single Quotest you cannot use Line Brakes 
+
+# EXAMPLE
+
+puts 'Hello\nWorld';
+
+puts
+
+# In Double Quotes you cannot use Interpolations
+
+text = "We all live in a yelow submarine";
+puts '#{text}';
