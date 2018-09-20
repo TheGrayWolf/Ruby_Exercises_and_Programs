047598c5f4765222103cd9f738c28fd9abce5782
Alin Craciunescu <alin.craciunescu@yahoo.com>
2018-09-20 09:57:14 +0300

    Length and size methods

---
+++ Lenght_&_Size_methods.rb
@@ -0,0 +1,21 @@
+
+a = "Hello world"
+b = "Hi, my name is Alin!"
+c = " "
+d = ""
+
+# The lenght method gives us the number of caracters in a string
+# Even Space is considerd a character
+p a.length
+p b.length
+p c.length
+p d.length
+
+puts
+
+# The lenght method gives us the number of caracters in a string
+# Even Space is considerd a character
+p a.size
+p b.size
+p c.size
+p d.size
