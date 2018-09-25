20f808d01735c1cec64f9098ad487bf7e1b10417
Alin Craciunescu <alin.craciunescu@yahoo.com>
2018-09-25 12:18:16 +0300

    How To overide a Character

---
+++ Overide_a_character.rb
@@ -0,0 +1,15 @@
+thing = "a huge potato";
+
+p thing;
+
+puts
+# To replace a character you write the character number in [] and what you want to replace them with
+thing[0] = "i want"
+
+p thing;
+
+puts
+# To replace a series of characters [a, b] from a to b
+
+thing[2, 4] = "need"
+p thing
