9cceb5ad65e6eae7f19668f6b7d3c64da5b7e16c
Alin Craciunescu <alin.craciunescu@yahoo.com>
2018-09-25 12:47:30 +0300

    How to change the case ( capitalization )

---
+++ Case_Method.rb
@@ -0,0 +1,13 @@
+# Capitalize rewrites the string and Capitalizes the first letter and puts the rest of the string in uncapitalized
+
+puts "hello".capitalize
+puts "hello World".capitalize
+
+puts
+# Upcase / Downcase makes the entire string to uppcase or downcase
+puts "hello".upcase
+puts "HeLlO".downcase
+
+puts
+# Swapcase switches the casing of the letter
+puts "hELLO".swapcase
